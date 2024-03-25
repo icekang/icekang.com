d87e45218cf5908fa0b4526625e41f2d3bf78133
Naravich Chutisilp <namkang_chutisilp@hotmail.com>
2024-03-25 02:39:36 -0400

    style: a tag

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,7 +16,7 @@
 	<div class="title grid grid-cols-3 gap-4">
 		<div class="col-span-2">
 			<h1>Naravich Chutisilp</h1>
-			<p>I like computer vision and anything deep learning related. Currently, I am completing my Master's thesis at MIT in the <span><a href="https://imes.mit.edu/">Institute for Medical Engineering and Science (IMES).</a></span> Oh! By the way, I am a gym rat and a casual chef.</p>
+			<p>I like computer vision and anything deep learning related.Currently, I am completing my Master's thesis at MIT in the <span><a href="https://imes.mit.edu/">Institute for Medical Engineering and Science (IMES).</a></span> Oh! By the way, I am a gym rat and a casual chef.</p>
 		</div>
 		<IconSwiper images={images} />
 	</div>
@@ -31,6 +31,17 @@
 	.container {
 		@apply mx-auto;
 		@apply p-12;
+	}
+
+	a {
+		@apply text-blue-500;
+	}
+	a:hover {
+		@apply text-blue-700;
+	}
+
+	a:active {
+		@apply text-blue-900;
 	}
 
 	h1 {
