a1bc64000cb6af70b038b1cdd8f2e991cc01ddff
Naravich Chutisilp <namkang_chutisilp@hotmail.com>
2024-03-25 02:39:07 -0400

    feat: add some real projects

--- src/lib/components/List.svelte
+++ src/lib/components/List.svelte
@@ -1,45 +1,36 @@
 <script lang="ts">
-	// Filterable list
-
+	import { CATEGORY } from '$lib/components/List.ts';
 	let items = [
-		{ id: 1, name: 'apple', category: 'fruit' },
-		{ id: 2, name: 'banana', category: 'fruit' },
-		{ id: 3, name: 'carrot', category: 'vegetable' },
-		{ id: 4, name: 'date', category: 'fruit' },
-		{ id: 5, name: 'eggplant', category: 'vegetable' },
-		{ id: 6, name: 'fig', category: 'fruit' },
-		{ id: 7, name: 'grape', category: 'fruit' },
-		{ id: 8, name: 'honeydew', category: 'fruit' },
-		{ id: 9, name: 'iceberg lettuce', category: 'vegetable' },
-		{ id: 10, name: 'jackfruit', category: 'fruit' },
-		{ id: 11, name: 'kiwi', category: 'fruit' },
-		{ id: 12, name: 'lemon', category: 'fruit' },
-		{ id: 13, name: 'mango', category: 'fruit' },
-		{ id: 14, name: 'nectarine', category: 'fruit' },
-		{ id: 15, name: 'orange', category: 'fruit' },
-		{ id: 16, name: 'pear', category: 'fruit' },
-		{ id: 17, name: 'quince', category: 'fruit' },
-		{ id: 18, name: 'raspberry', category: 'fruit' },
-		{ id: 19, name: 'strawberry', category: 'fruit' },
-		{ id: 20, name: 'tomato', category: 'fruit' },
-		{ id: 21, name: 'ugli fruit', category: 'fruit' },
-		{ id: 22, name: 'vanilla bean', category: 'fruit' },
-		{ id: 23, name: 'watermelon', category: 'fruit' },
-		{ id: 24, name: 'xigua', category: 'fruit' },
-		{ id: 25, name: 'yellow squash', category: 'vegetable' },
-		{ id: 26, name: 'zucchini', category: 'vegetable' }
+		{
+			name: 'Multi-Modal Self-Supervised Learning For 3D Medical Image Segmentation Of Cardio Calcified Plaques',
+			categories: [CATEGORY.COMPUTER_VISION, CATEGORY.DEEP_LEARNING]
+		},
+		{
+			name: 'TTool: A Supervised Artificial Intelligence-Assisted Visual Pose Detector for Tool Heads in Augmented Reality Woodworking',
+			categories: [CATEGORY.SOFTWARE_ENGINEERING, CATEGORY.COMPUTER_VISION]
+		},
+		{
+			name: 'AI4TB: An Efficient Edge-Device Interpretable Chest X-Ray Tuberculosis Detection',
+			categories: [
+				CATEGORY.COMPUTER_VISION,
+				CATEGORY.DEEP_LEARNING,
+				CATEGORY.MACHINE_LEARNING_ENGINEER,
+				CATEGORY.SOFTWARE_ENGINEERING
+			]
+		},
 	];
 
-	let tags = items.reduce((accumulator: { name: string; selected: boolean }[], item) => {
-		let category = item.category.toLowerCase();
-		if (!accumulator.some((tag) => tag.name === category)) {
-			accumulator.push({ name: category, selected: true });
-		}
-		return accumulator;
-	}, []);
+	let tags = items
+		.flatMap((item) => item.categories)
+		.reduce((acc: { name: string; selected: boolean }[], category) => {
+			if (!acc.find((tag) => tag.name === category)) {
+				acc.push({ name: category, selected: true });
+			}
+			return acc;
+		}, []);
 
 	$: filteredItems = items.filter((item) => {
-		return tags.find((tag) => tag.selected && tag.name === item.category);
+		return tags.find((tag) => tag.selected && item.categories.includes(tag.name));
 	});
 </script>
 
@@ -47,11 +38,11 @@
 	<div class="flex flex-row gap-4 my-2">
 		{#each tags as tag}
 			<button
-                class:bg-gray-500={!tag.selected}
-                class:hover:bg-gray-700={!tag.selected}
-                class:bg-blue-500={tag.selected}
-                class:hover:bg-blue-700={tag.selected}
-                class="text-sm text-white font-bold py-2 px-4 rounded-full"
+				class:bg-gray-500={!tag.selected}
+				class:hover:bg-gray-700={!tag.selected}
+				class:bg-blue-500={tag.selected}
+				class:hover:bg-blue-700={tag.selected}
+				class="text-sm text-white font-bold py-2 px-4 rounded-full"
 				on:click={() => {
 					tag.selected = !tag.selected;
 				}}>{tag.name}</button
---
+++ src/lib/components/List.ts
@@ -0,0 +1,7 @@
+export const CATEGORY = {
+    COMPUTER_VISION: "computer vision",
+    DEEP_LEARNING: "deep learning",
+    SOFTWARE_ENGINEERING: "software engineering",
+    DATA_ENGINEERING: "data engineering",
+    MACHINE_LEARNING_ENGINEER: "ML engineer",
+}
