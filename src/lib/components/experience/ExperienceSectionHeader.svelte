5c9511720c917dbfa15cca7afa301438da694bb1
Naravich Chutisilp <namkang_chutisilp@hotmail.com>
2026-05-10 01:42:39 +0200

    feat: add experience section header and sidebar components

--- src/lib/components/experience/ExperienceSectionHeader.svelte
+++ src/lib/components/experience/ExperienceSectionHeader.svelte
@@ -2,8 +2,10 @@
 	export let title: string;
 </script>
 
-<div class="border-b-2 border-black bg-surface sticky top-[74px] z-40">
-	<h2 class="font-headline-xl text-[80px] lg:text-[120px] leading-[0.8] uppercase font-black tracking-tighter p-8">
+<div class="border-b-2 border-black bg-surface sticky top-[74px] z-40 overflow-hidden">
+	<h2
+		class="font-headline-xl text-6xl md:text-7xl lg:text-[80px] xl:text-[100px] 2xl:text-[120px] leading-[0.8] uppercase font-black tracking-tighter p-6 md:p-8 break-words"
+	>
 		{title}
 	</h2>
 </div>
