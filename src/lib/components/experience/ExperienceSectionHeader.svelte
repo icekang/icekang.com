b7fd254f32c9683c34adda4e2770821526161694
Naravich Chutisilp <namkang_chutisilp@hotmail.com>
2026-05-10 01:27:37 +0200

    fix: gap between the experience section header and the top nav bar

--- src/lib/components/experience/ExperienceSectionHeader.svelte
+++ src/lib/components/experience/ExperienceSectionHeader.svelte
@@ -2,7 +2,7 @@
 	export let title: string;
 </script>
 
-<div class="border-b-2 border-black bg-surface sticky top-[82px] z-40">
+<div class="border-b-2 border-black bg-surface sticky top-[74px] z-40">
 	<h2 class="font-headline-xl text-[80px] lg:text-[120px] leading-[0.8] uppercase font-black tracking-tighter p-8">
 		{title}
 	</h2>
