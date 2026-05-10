5845e4679b19253511ebecc464c73542d3b5a84b
Naravich Chutisilp <namkang_chutisilp@hotmail.com>
2026-05-10 02:43:41 +0200

    ui: ensure no word break

--- src/lib/components/experience/ExperienceSectionHeader.svelte
+++ src/lib/components/experience/ExperienceSectionHeader.svelte
@@ -4,7 +4,7 @@
 
 <div class="border-b-2 border-black bg-surface sticky top-[74px] z-40 overflow-hidden">
 	<h2
-		class="font-headline-xl text-6xl md:text-7xl lg:text-[80px] xl:text-[100px] 2xl:text-[120px] leading-[0.8] uppercase font-black tracking-tighter p-6 md:p-8 break-words"
+		class="font-headline-xl text-[9.5vw] min-[420px]:text-[48px] sm:text-6xl md:text-7xl lg:text-[80px] xl:text-[100px] 2xl:text-[120px] leading-[0.8] uppercase font-black tracking-tighter pt-8 pb-2 px-6 md:pt-12 md:pb-4 md:px-8 break-words"
 	>
 		{title}
 	</h2>
