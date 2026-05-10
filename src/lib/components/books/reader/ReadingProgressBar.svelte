80c5a8223e0643f7086c66220b3e60e33032160e
Naravich Chutisilp <namkang_chutisilp@hotmail.com>
2026-05-10 14:50:02 +0200

    fix: reading process width overflowing

--- src/lib/components/books/reader/ReadingProgressBar.svelte
+++ src/lib/components/books/reader/ReadingProgressBar.svelte
@@ -2,15 +2,11 @@
 	export let scrollProgress: number = 0;
 </script>
 
-<div class="fixed top-[72px] left-0 w-full z-[60] pointer-events-none">
-	<div class="mx-auto px-margin-mobile md:px-margin-desktop">
+<div class="sticky full-width top-[73px] z-[60] pointer-events-none">
+	<div class="h-1.5 bg-gray-100/50 relative overflow-hidden pointer-events-auto">
 		<div
-			class="h-1.5 bg-gray-100/50 border-x border-b border-black relative overflow-hidden pointer-events-auto"
-		>
-			<div
-				class="absolute top-0 left-0 h-full bg-surface-accent transition-all duration-150 ease-out"
-				style="width: {scrollProgress}%"
-			></div>
-		</div>
+			class="absolute top-0 left-0 h-full bg-surface-accent transition-all duration-150 ease-out"
+			style="width: {scrollProgress}%"
+		></div>
 	</div>
 </div>
